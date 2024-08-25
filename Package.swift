23d80fc705d7b6e22bcf99e51a17ce2ef6e2bf78
asani <asani@asanis-MacBook-Pro-2.local>
2024-08-25 20:38:35 +0700

    update code

--- Package.swift
+++ Package.swift
@@ -1,20 +1,26 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "KMPSPM",
-    platforms: [
-        .iOS(.v14)
-    ],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KMPSPM",
-            targets: ["KMPSPM"]
-        ),
+            targets: ["KMPSPM"]),
+    ],
+    dependencies: [
     ],
     targets: [
-        .binaryTarget(
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
             name: "KMPSPM",
-            path: "./KMPSPM.xcframework"
+            dependencies: [
+            ]
         ),
     ]
 )

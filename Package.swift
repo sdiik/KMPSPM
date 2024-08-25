74ee2dd37fcd26c5f5bf58ce329762aaad1b7325
asani <asani@asanis-MacBook-Pro-2.local>
2024-08-25 20:41:51 +0700

    update code

--- Package.swift
+++ Package.swift
@@ -17,10 +17,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
+        .binaryTarget(
             name: "KMPSPM",
-            dependencies: [
-            ]
+            path: "./KMPSPM.xcframework"
         ),
+    ]
     ]
 )
